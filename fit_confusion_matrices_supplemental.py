5862b70cde1e7cb51130c97a3821875c177762ad
craigsanders <crasanders@gmail.com>
2018-10-22 17:16:30 -0500

    Working 'GCM_Sup'

--- fit_confusion_matrices_supplemental.py
+++ fit_confusion_matrices_supplemental.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.optimize import basinhopping, minimize
 from os.path import join
-from crasanders.gcm import GCM, GCM_Sup, GCM_Sigmoid
+from crasanders.gcm import GCM, GCM_Sup
 import pickle
 
 data_dir = 'data'
@@ -15,8 +15,8 @@ nconditions = len(conditions)
 
 representations = {
     'mds_sup': np.loadtxt(join(data_dir, 'mds_120_supplemental_dims.txt')),
-    'cnn_sup': np.hstack((np.loadtxt(join(data_dir, 'cnn_120.txt')),
-                          np.loadtxt(join(data_dir, '120_predictions_supplemental_dims.txt'))[:, -5:]))
+    # 'cnn_sup': np.hstack((np.loadtxt(join(data_dir, 'cnn_120.txt')),
+    #                       np.loadtxt(join(data_dir, '120_predictions_supplemental_dims.txt'))[:, -5:]))
 }
 
 cm = {
@@ -45,62 +45,6 @@ for rep in representations:
         stim[rep][cond] = representations[rep][np.isin(cats, categories[cond]),:]
         exemplars[rep][cond] = representations[rep][np.logical_and(training, np.isin(cats, categories[cond])),:]
 
-def fit_full_sigmoid(parms, args):
-    rep, fitted = args
-    fit = 0
-    offset1 = 1 + n_sup*2
-    offset2 = offset1 + nbiases * nconditions
-    predictions = []
-    for cond in conditions:
-        gcm = GCM_Sigmoid(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=parms[0],
-                          biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2],
-                          supp=start_sup, K=parms[1:n_sup+1], refs=parms[n_sup+1:offset1])
-        fit += gcm.log_likelihood(stim[rep][cond], cm[cond], include_factorial=fitted)
-        predictions.append(gcm.predict(stim[rep][cond]))
-        offset1 += nbiases
-        offset2 += nweights
-    if not fitted:
-        return -fit
-    else:
-        return [-fit, predictions]
-
-fits = {}
-for rep in representations:
-    print('fitting:', rep)
-
-    offset = 1 + n_sup*2
-    nweights = representations[rep].shape[1]
-    parm = [1.] + [1.]*n_sup + [0.]*n_sup + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
-
-    fit = minimize(fit_full_sigmoid, parm, args=[rep, False],
-                   bounds=[(0, None)]*(2*n_sup+1) + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
-                   constraints=[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}],
-                    options={'maxiter':10000})
-
-    fit.n_log_lik, fit.predictions = fit_full_sigmoid(fit.x, args=[rep, True])
-
-    fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3 + n_sup*2
-    fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
-
-    fits[rep] = fit
-
-with open(join(data_dir, 'fits_full_sigmoid.pkl'), 'wb') as f:
-    pickle.dump(fits, f)
-
-for rep in representations:
-    print(rep)
-    fit = fits[rep]
-    print('-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
-    print('parameters:', fit.x)
-    print()
-
-
-
 def fit_full_sup(parms, args):
     rep, fitted = args
     fit = 0
@@ -120,23 +64,36 @@ def fit_full_sup(parms, args):
     else:
         return [-fit, predictions]
 
+niter = 10
+
 fits = {}
 for rep in representations:
     print('fitting:', rep)
 
     offset = 4 + n_sup
     nweights = representations[rep].shape[1]
-    parm = [1.] + [1., 1., 1.] + [0.]*n_sup + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
 
-    fit = minimize(fit_full_sup, parm, args=[rep, False],
-                   bounds=[(0, None), (0, None)] + [(None, None)]*2 + [(0, None)]*n_sup + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
-                   constraints=[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}],
-                    options={'maxiter':10000})
+    lowest = np.inf
+    for i in range(niter):
+        c = np.random.randint(10)
+        u = np.random.randint(10)
+        v = np.random.randint(-5, 5)
+        w = np.random.randint(-5, 5)
+
+        biases = []
+        for
+        biases = [np.random()]
+
+        parm = [2.] + [3., 1., 1.] + [3.]*n_sup + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
+
+        fit = minimize(fit_full_sup, parm, args=[rep, False],
+                       bounds=[(0, None), (0, None)] + [(None, None)]*2 + [(0, None)]*n_sup + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
+                       constraints=[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
+                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
+                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
+                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
+                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
+                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}])
 
     fit.n_log_lik, fit.predictions = fit_full_sup(fit.x, args=[rep, True])
 
@@ -145,12 +102,73 @@ for rep in representations:
 
     fits[rep] = fit
 
-with open(join(data_dir, 'fits_full_supp.pkl'), 'wb') as f:
-    pickle.dump(fits, f)
+# with open(join(data_dir, 'fits_full_supp.pkl'), 'wb') as f:
+#     pickle.dump(fits, f)
 
 for rep in representations:
     print(rep)
     fit = fits[rep]
-    print('-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
-    print('parameters:', fit.x)
+    print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
     print()
+
+
+# def fit_partial_sup(parms, args):
+#     rep, fitted, dim = args
+#     fit = 0
+#     offset1 = 5
+#     offset2 = offset1 + nbiases * nconditions
+#     predictions = []
+#     for cond in conditions:
+#         ex = np.column_stack((exemplars[rep][cond][:, :8], exemplars[rep][cond][:, dim]))
+#         st = np.column_stack((stim[rep][cond][:,:8], stim[rep][cond][:,dim]))
+#         gcm = GCM_Sup(nbiases, nweights, 20, ex, strengths, c=parms[0],
+#                           biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2],
+#                           supp=8, u=parms[1], v=parms[2], w=parms[3], refs=[parms[4]])
+#         fit += gcm.log_likelihood(st, cm[cond], include_factorial=fitted)
+#         predictions.append(gcm.predict(st))
+#         offset1 += nbiases
+#         offset2 += nweights
+#     if not fitted:
+#         return -fit
+#     else:
+#         return [-fit, predictions]
+#
+# partial_fits = {}
+# for rep in representations:
+#     partial_fits[rep] = {}
+#     for dim in range(8,13):
+#         print('fitting:', rep, 'dimension:', dim)
+#
+#         offset = 5
+#         nweights = 9
+#         parm = [1.] + [1., 1., 1., 0] + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
+#
+#         fit = minimize(fit_partial_sup, parm, args=[rep, False, dim],
+#                        bounds=[(0, None), (0, None)] + [(None, None)]*2 + [(0, None)] + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
+#                        constraints=[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}],
+#                         options={'maxiter':10000})
+#
+#         fit.n_log_lik, fit.predictions = fit_partial_sup(fit.x, args=[rep, True, dim])
+#
+#         fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3 + 4
+#         fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
+#
+#         partial_fits[rep][dim] = fit
+#
+# # with open(join(data_dir, 'fits_partial_supp.pkl'), 'wb') as f:
+# #     pickle.dump(partial_fits, f)
+#
+# base_bic = {'mds_sup': 8115, 'cnn_sup': 10059}
+# for rep in representations:
+#     print(rep)
+#     for dim in range(8, 13):
+#         print(dim)
+#         fit = partial_fits[rep][dim]
+#         print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
+#         # print('parameters:', fit.x)
+#         print()
