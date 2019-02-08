21428ec529df284bbe514c13114d9ebefa3e94d8
craigsanders <crasanders@gmail.com>
2019-02-08 17:33:02 -0600

    updating fits

--- predict_similarity.py
+++ predict_similarity.py
@@ -4,19 +4,19 @@ from scipy.optimize import nnls
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 
-features = np.loadtxt('data/vgg16_features_120.txt')
+features = np.loadtxt('data/resnet50_features_120.txt')
 
 dotproduct = features @ features.transpose()
 dotproduct = dotproduct[np.triu(dotproduct, k=1).astype(bool)]
 
 obs_sim = np.array(pd.read_csv('data/similarity_120.csv', header=None))
-obs_sim = obs_sim[np.triu(obs_sim, k=1).astype(bool)]
+obs_sim = 10 - obs_sim[np.triu(obs_sim, k=1).astype(bool)]
 
-features_360 = np.loadtxt('data/vgg16_features_360.txt')
+features_360 = np.loadtxt('data/resnet50_features_360.txt')
 n, dim = features_360.shape
-x = np.array([features_360[i,:] * features_360[j,:] for i in range(n-1) for j in range(i+1, n)])
+x = np.array([(features_360[i,:] - features_360[j,:])**2 for i in range(n-1) for j in range(i+1, n)])
 y = np.array(pd.read_csv('data/similarity_360.csv', header=None))
-y = y[np.triu(y, k=1).astype(bool)]
+y = 10 - y[np.triu(y, k=1).astype(bool)]
 
 x = x[~np.isnan(y)]
 y = y[~np.isnan(y)]
@@ -51,12 +51,12 @@ X = np.vstack((x, best_lam*np.eye(dim)))
 Y = np.concatenate((y, [0] * dim))
 w, loss = nnls(X, Y)
 
-transf_features = features * np.sqrt(w)
+transf_features = features * w
 transf_dot = transf_features @ transf_features.transpose()
 transf_dot = transf_dot[np.triu(transf_dot, k=1).astype(bool)]
 
 sims = pd.DataFrame({'Raw': dotproduct, 'Transformed': transf_dot, 'Observed': obs_sim})
 print(sims.corr())
 
-np.savetxt('vgg16_transformed_120.txt', transf_features, fmt='%.18f')
+np.savetxt('resnet50_transformed2_120.txt', transf_features, fmt='%.18f')
 
