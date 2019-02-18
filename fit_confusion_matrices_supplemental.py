6fca27f4918f9f1cb3d27a65c61321e496c24e47
craigsanders <crasanders@gmail.com>
2019-02-18 15:04:55 -0600

    added gamma to models

--- fit_confusion_matrices_supplemental.py
+++ fit_confusion_matrices_supplemental.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.optimize import basinhopping, minimize
 from os.path import join
-from crasanders.gcm import GCM, GCM_Sup
+from crasanders.gcm import GCM, GCM_Sup, GCM_cw
 import pickle
 
 data_dir = 'data'
@@ -14,9 +14,13 @@ conditions = ["Igneous", "Metamorphic", "Mixed"]
 nconditions = len(conditions)
 
 representations = {
-    'mds_sup': np.loadtxt(join(data_dir, 'mds_120_supplemental_dims.txt')),
-    'cnn_sup': np.hstack((np.loadtxt(join(data_dir, 'cnn_120.txt')),
-                          np.loadtxt(join(data_dir, '120_predictions_supplemental_dims.txt'))[:, -5:]))
+    # 'mds_sup': np.loadtxt(join(data_dir, 'mds_120_supplemental_dims.txt')),
+    # 'cnn_sup': np.hstack((np.loadtxt(join(data_dir, 'cnn_120.txt')),
+    #                       np.loadtxt(join(data_dir, '120_predictions_supplemental_dims.txt'))[:, -5:]))
+    # 'mds': np.loadtxt(join(data_dir, 'mds_120.txt')),
+    # 'cnn': np.loadtxt(join(data_dir, 'cnn_120.txt')),
+    'mds_rand': np.hstack((np.loadtxt(join(data_dir, 'mds_120.txt')), np.random.random(120)*10 - 5)),
+    'cnn_rand': np.hstack((np.loadtxt(join(data_dir, 'mds_120.txt')), np.random.random(120)*10 - 5)))
 }
 
 cm = {
@@ -45,120 +49,216 @@ for rep in representations:
         stim[rep][cond] = representations[rep][np.isin(cats, categories[cond]),:]
         exemplars[rep][cond] = representations[rep][np.logical_and(training, np.isin(cats, categories[cond])),:]
 
-def fit_full_sup(parms, args):
+# def fit_full_sup(parms, args):
+#     rep, fitted = args
+#     fit = 0
+#     offset1 = 4 + n_sup
+#     offset2 = offset1 + nbiases * nconditions
+#     predictions = []
+#     for cond in conditions:
+#         gcm = GCM_Sup(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=parms[0],
+#                           biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2],
+#                           supp=start_sup, u=parms[1], v=parms[2], w=parms[3], refs=parms[4:offset1])
+#         fit += gcm.log_likelihood(stim[rep][cond], cm[cond], include_factorial=fitted)
+#         predictions.append(gcm.predict(stim[rep][cond]))
+#         offset1 += nbiases
+#         offset2 += nweights
+#     if not fitted:
+#         return -fit
+#     else:
+#         return [-fit, predictions]
+#
+# fits = {}
+# for rep in representations:
+#     print('fitting:', rep)
+#
+#     offset = 4 + n_sup
+#     nweights = representations[rep].shape[1]
+#
+#
+#     parm = [1.] + [1., 1., 1.] + [0]*n_sup + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
+#
+#     fit = minimize(fit_full_sup, parm, args=[rep, False],
+#                    bounds=[(0, None), (0, None)] + [(None, None)]*2 + [(-5, 5)]*n_sup + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
+#                    constraints=[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}])
+#
+#     fit.n_log_lik, fit.predictions = fit_full_sup(fit.x, args=[rep, True])
+#
+#     fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3 + n_sup + 3
+#     fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
+#
+#     fits[rep] = fit
+#
+# with open(join(data_dir, 'fits_full_supp.pkl'), 'wb') as f:
+#     pickle.dump(fits, f)
+#
+# for rep in representations:
+#     print(rep)
+#     fit = fits[rep]
+#     print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
+#     print()
+#
+#
+# def fit_partial_sup(parms, args):
+#     rep, fitted, dim = args
+#     fit = 0
+#     offset1 = 5
+#     offset2 = offset1 + nbiases * nconditions
+#     predictions = []
+#     for cond in conditions:
+#         ex = np.column_stack((exemplars[rep][cond][:, :8], exemplars[rep][cond][:, dim]))
+#         st = np.column_stack((stim[rep][cond][:,:8], stim[rep][cond][:,dim]))
+#         gcm = GCM_Sup(nbiases, nweights, 20, ex, strengths, c=parms[0],
+#                           biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2],
+#                           supp=8, u=parms[1], v=parms[2], w=parms[3], refs=[parms[4]])
+#         fit += gcm.log_likelihood(st, cm[cond], include_factorial=fitted)
+#         predictions.append(gcm.predict(st))
+#         offset1 += nbiases
+#         offset2 += nweights
+#     if not fitted:
+#         return -fit
+#     else:
+#         return [-fit, predictions]
+#
+# partial_fits = {}
+# for rep in representations:
+#     partial_fits[rep] = {}
+#     for dim in range(8,13):
+#         print('fitting:', rep, 'dimension:', dim)
+#
+#         offset = 5
+#         nweights = 9
+#         parm = [1.] + [1., 1., 1., 0] + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
+#
+#         fit = basinhopping(fit_partial_sup, parm, minimizer_kwargs={'args':[rep, False, dim],
+#                        'bounds':[(0, None), (0, None)] + [(None, None)]*2 + [(-5, 5)] + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
+#                        'constraints':[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
+#                          {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}]}
+#                        )
+#
+#         fit.n_log_lik, fit.predictions = fit_partial_sup(fit.x, args=[rep, True, dim])
+#
+#         fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3 + 4
+#         fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
+#
+#         partial_fits[rep][dim] = fit
+#
+# with open(join(data_dir, 'fits_partial_supp_bh.pkl'), 'wb') as f:
+#     pickle.dump(partial_fits, f)
+#
+# for rep in representations:
+#     print(rep)
+#     for dim in range(8, 13):
+#         fit = partial_fits[rep][dim]
+#         print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
+#         print()
+#
+# with open(join(data_dir, 'fits_full_supp.pkl'), 'rb') as f:
+#     old_fits = pickle.load(f)
+
+
+
+# def fit_full(parms, args):
+#     rep, fitted = args
+#     fit = 0
+#     offset1 = 1
+#     offset2 = offset1 + nbiases * nconditions
+#     predictions = []
+#     for cond in conditions:
+#         gcm = GCM(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=parms[0],
+#                           biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2])
+#         fit += gcm.log_likelihood(stim[rep][cond], cm[cond], include_factorial=fitted)
+#         predictions.append(gcm.predict(stim[rep][cond]))
+#         offset1 += nbiases
+#         offset2 += nweights
+#     if not fitted:
+#         return -fit
+#     else:
+#         return [-fit, predictions]
+#
+# fits = {}
+# for rep in representations:
+#     print('fitting:', rep)
+#
+#     offset = 1
+#     nweights = representations[rep].shape[1]
+#     print(1 + (nbiases-1)*3 + (nweights-1)*3, 'free parameters')
+#
+#     parm = [1.] + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
+#
+#     fit = basinhopping(fit_full, parm, minimizer_kwargs={'args':[rep, False],
+#                    'bounds':[(0, None)] + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
+#                    'constraints':[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
+#                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}]})
+#
+#     fit.n_log_lik, fit.predictions = fit_full(fit.x, args=[rep, True])
+#
+#     fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3
+#     fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
+#
+#     fits[rep] = fit
+#
+# with open(join(data_dir, 'fits_original.pkl'), 'wb') as f:
+#     pickle.dump(fits, f)
+#
+
+
+def fit_full(parms, args):
     rep, fitted = args
     fit = 0
-    offset1 = 4 + n_sup
+    offset1 = 2
     offset2 = offset1 + nbiases * nconditions
     predictions = []
     for cond in conditions:
-        gcm = GCM_Sup(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=parms[0],
-                          biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2],
-                          supp=start_sup, u=parms[1], v=parms[2], w=parms[3], refs=parms[4:offset1])
-        fit += gcm.log_likelihood(stim[rep][cond], cm[cond], include_factorial=fitted)
-        predictions.append(gcm.predict(stim[rep][cond]))
+        gcm = GCM_cw(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=[parms[0], parms[1]],
+                          biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2])
+        fit += gcm.log_likelihood(stim[rep][cond], cats[:40], cm[cond], include_factorial=fitted)
+        predictions.append(gcm.predict(stim[rep][cond], cats[:40]))
         offset1 += nbiases
         offset2 += nweights
     if not fitted:
         return -fit
     else:
         return [-fit, predictions]
-
-niter = 10
 
 fits = {}
 for rep in representations:
     print('fitting:', rep)
 
-    offset = 4 + n_sup
+    offset = 2
     nweights = representations[rep].shape[1]
+    print(2 + (nbiases-1)*3 + (nweights-1)*3, 'free parameters')
 
+    parm = [1., 1.] + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
 
-    parm = [1.] + [1., 1., 1.] + [0]*n_sup + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
-
-    fit = minimize(fit_full_sup, parm, args=[rep, False],
-                   bounds=[(0, None), (0, None)] + [(None, None)]*2 + [(-5, 5)]*n_sup + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
-                   constraints=[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
+    fit = basinhopping(fit_full, parm, minimizer_kwargs={'args':[rep, False],
+                   'bounds':[(0, None), (0, None)] + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
+                   'constraints':[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
                      {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
-                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}])
+                     {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}]})
 
-    fit.n_log_lik, fit.predictions = fit_full_sup(fit.x, args=[rep, True])
+    fit.n_log_lik, fit.predictions = fit_full(fit.x, args=[rep, True])
 
-    fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3 + n_sup + 3
+    fit.free_parm = 2 + (nbiases-1)*3 + (nweights-1)*3
     fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
 
     fits[rep] = fit
 
-with open(join(data_dir, 'fits_full_supp.pkl'), 'wb') as f:
+with open(join(data_dir, 'fits_original_cw.pkl'), 'wb') as f:
     pickle.dump(fits, f)
-
-for rep in representations:
-    print(rep)
-    fit = fits[rep]
-    print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
-    print()
-
-
-def fit_partial_sup(parms, args):
-    rep, fitted, dim = args
-    fit = 0
-    offset1 = 5
-    offset2 = offset1 + nbiases * nconditions
-    predictions = []
-    for cond in conditions:
-        ex = np.column_stack((exemplars[rep][cond][:, :8], exemplars[rep][cond][:, dim]))
-        st = np.column_stack((stim[rep][cond][:,:8], stim[rep][cond][:,dim]))
-        gcm = GCM_Sup(nbiases, nweights, 20, ex, strengths, c=parms[0],
-                          biases=parms[offset1:nbiases+offset1], weights=parms[offset2:nweights+offset2],
-                          supp=8, u=parms[1], v=parms[2], w=parms[3], refs=[parms[4]])
-        fit += gcm.log_likelihood(st, cm[cond], include_factorial=fitted)
-        predictions.append(gcm.predict(st))
-        offset1 += nbiases
-        offset2 += nweights
-    if not fitted:
-        return -fit
-    else:
-        return [-fit, predictions]
-
-partial_fits = {}
-for rep in representations:
-    partial_fits[rep] = {}
-    for dim in range(8,13):
-        print('fitting:', rep, 'dimension:', dim)
-
-        offset = 5
-        nweights = 9
-        parm = [1.] + [1., 1., 1., 0] + [1/nbiases]*nbiases*nconditions + [1/nweights]*nweights*nconditions
-
-        fit = basinhopping(fit_partial_sup, parm, minimizer_kwargs={'args':[rep, False, dim],
-                       'bounds':[(0, None), (0, None)] + [(None, None)]*2 + [(-5, 5)] + [(0,1)]*nbiases*nconditions + [(0,1)]*nweights*nconditions,
-                       'constraints':[{'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[offset:nbiases+offset])},
-                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[nbiases+offset:2*nbiases+offset])},
-                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[2*nbiases+offset:3*nbiases+offset])},
-                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+offset:3*nbiases+nweights+offset])},
-                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+nweights+offset:3*nbiases+2*nweights+offset])},
-                         {'type': 'eq', 'fun': lambda parms: 1- np.sum(parms[3*nbiases+2*nweights+offset:3*nbiases+3*nweights+offset])}]}
-                       )
-
-        fit.n_log_lik, fit.predictions = fit_partial_sup(fit.x, args=[rep, True, dim])
-
-        fit.free_parm = 1 + (nbiases-1)*3 + (nweights-1)*3 + 4
-        fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
-
-        partial_fits[rep][dim] = fit
-
-with open(join(data_dir, 'fits_partial_supp_bh.pkl'), 'wb') as f:
-    pickle.dump(partial_fits, f)
-
-for rep in representations:
-    print(rep)
-    for dim in range(8, 13):
-        fit = partial_fits[rep][dim]
-        print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
-        print()
-
-with open(join(data_dir, 'fits_full_supp.pkl'), 'rb') as f:
-    old_fits = pickle.load(f)
--- fit_gcm_basic.py
+++ fit_gcm_basic.py
@@ -5,6 +5,18 @@ from crasanders.gcm import GCM
 from scipy.optimize import minimize, basinhopping
 import pickle
 
+from sklearn.preprocessing import scale
+
+class MyBounds(object):
+    def __init__(self, xmax=[20,5], xmin=[0,0] ):
+        self.xmax = np.array(xmax)
+        self.xmin = np.array(xmin)
+    def __call__(self, **kwargs):
+        x = kwargs["x_new"]
+        tmax = bool(np.all(x <= self.xmax))
+        tmin = bool(np.all(x >= self.xmin))
+        return tmax and tmin
+
 data_dir = 'data'
 nbiases = 10
 nweights = 9
@@ -18,15 +30,14 @@ mds_rand = (np.random.random(120)*10 - 5).reshape(120,1)
 cnn_rand = (np.random.random(120)*10 - 5).reshape(120,1)
 
 representations = {
-    # 'mds_sup': np.loadtxt(join(data_dir, 'mds_120_supplemental_dims.txt'))[:,[i < 8 or i == 11 for i in range(13)]]
-    # 'cnn_sup': np.hstack((np.loadtxt(join(data_dir, 'cnn_120.txt')),
-    #                       np.loadtxt(join(data_dir, '120_predictions_supplemental_dims.txt'))[:, -5:]))
     'mds': np.loadtxt(join(data_dir, 'mds_120.txt')),
     'cnn': np.loadtxt(join(data_dir, 'cnn_120.txt')),
     # 'mds_rand': np.hstack((np.loadtxt(join(data_dir, 'mds_120.txt')), mds_rand))
     # 'cnn_rand': np.hstack((np.loadtxt(join(data_dir, 'cnn_120.txt')), cnn_rand))
-    'resnet50': np.loadtxt(join(data_dir, 'resnet50_features_120.txt')),
-    'resnet50_transformed': np.loadtxt(join(data_dir, 'resnet50_transformed_120.txt'))
+    # 'resnet50': np.loadtxt(join(data_dir, 'resnet50_features_120.txt')),
+    # 'resnet50_transformed': np.loadtxt(join(data_dir, 'resnet50_transformed_120.txt'))
+    # 'vgg16': np.loadtxt(join(data_dir, 'vgg16_features_120.txt')),
+    # 'vgg16_transformed': np.loadtxt(join(data_dir, 'vgg16_transformed_120.txt'))
 }
 
 cm = {
@@ -58,15 +69,15 @@ for rep in representations:
 def fit_gcm(parms, args):
     rep, fitted = args
     fit = 0
-    offset1 = 5
-    offset2 = offset1 + nbiases * nconditions
     predictions = []
     for cond in conditions:
-        gcm = GCM(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=parms[0])
+        gcm = GCM(nbiases, nweights, 20, exemplars[rep][cond], strengths, c=parms[0], gamma=parms[1])
         fit += gcm.log_likelihood(stim[rep][cond], cm[cond], include_factorial=fitted)
         predictions.append(gcm.predict(stim[rep][cond]))
-        offset1 += nbiases
-        offset2 += nweights
+
+    if np.isnan(fit):
+        return np.inf
+
     if not fitted:
         return -fit
     else:
@@ -76,22 +87,27 @@ def fit_gcm(parms, args):
 fits = {}
 for rep in representations:
     print('fitting:', rep)
-    parm = [1.]
+    if rep == 'resnet50':
+        parm = [.26, 1.]
+    if rep == 'resnet50_transformed':
+        parm = [14.6, 1.]
+    else:
+        parm = [.1, .1]
     nweights = representations[rep].shape[1]
-    fit = basinhopping(fit_gcm, parm, minimizer_kwargs={'args':[rep, False]})
+    fit = basinhopping(fit_gcm, parm, minimizer_kwargs={'args':[rep, False]}, accept_test=MyBounds())
     fit.n_log_lik, fit.predictions = fit_gcm(fit.x, args=[rep, True])
-    fit.free_parm = 1
+    fit.free_parm = len(parm)
     fit.bic = 2*fit.n_log_lik + fit.free_parm * logn
     fits[rep] = fit
 
-with open(join(data_dir, 'fits_gcm_basic.pkl'), 'wb') as f:
-    pickle.dump(fits, f)
+with open(join(data_dir, 'best_fits.pkl'), 'rb') as f:
+    best = pickle.load(f)
+
+with open(join(data_dir, 'best_fits.pkl'), 'wb') as f:
+    pickle.dump(best, f)
 
 for rep in representations:
     print(rep)
     fit = fits[rep]
     print('free parms:', fit.free_parm, '-ln(L):', fit.n_log_lik, 'BIC:', fit.bic)
     print()
-
-
-
