75b6692fdcf738cefb8cc89f6332f1e3776d7cf7
craigsanders <crasanders@gmail.com>
2019-04-03 20:55:41 -0500

    fixed bugs in ridge regression code

--- predict_similarity.py
+++ predict_similarity.py
@@ -3,16 +3,18 @@ import pandas as pd
 from scipy.optimize import nnls
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
-
-features = np.loadtxt('data/resnet50_features_120.txt')
-
-dotproduct = features @ features.transpose()
-dotproduct = dotproduct[np.triu(dotproduct, k=1).astype(bool)]
+from sklearn.metrics import r2_score
 
 obs_sim = np.array(pd.read_csv('data/similarity_120.csv', header=None))
 obs_sim = 10 - obs_sim[np.triu(obs_sim, k=1).astype(bool)]
 
 features_360 = np.loadtxt('data/resnet50_features_360.txt')
+features_120 = np.loadtxt('data/resnet50_features_120.txt')
+
+scaler = StandardScaler()
+features_360 = scaler.fit_transform(features_360)
+features_120 = scaler.transform(features_120)
+
 n, dim = features_360.shape
 x = np.array([(features_360[i,:] - features_360[j,:])**2 for i in range(n-1) for j in range(i+1, n)])
 y = np.array(pd.read_csv('data/similarity_360.csv', header=None))
@@ -21,18 +23,16 @@ y = 10 - y[np.triu(y, k=1).astype(bool)]
 x = x[~np.isnan(y)]
 y = y[~np.isnan(y)]
 
-scaler = StandardScaler()
+y = y ** 2
+
 kf = KFold(5)
 best_loss = np.inf
 best_lam = None
-for lam in np.logspace(-8, 2, 10):
+for i, lam in enumerate(np.logspace(2, 3, 10)):
     this_loss = 0
     for train, val in kf.split(x):
         x_train, x_val = x[train], x[val]
         y_train, y_val = y[train], y[val]
-
-        x_train = scaler.fit_transform(x_train)
-        x_val = scaler.transform(x_val)
 
         X = np.vstack((x_train, lam*np.eye(dim)))
         Y = np.concatenate((y_train, [0] * dim))
@@ -46,17 +46,16 @@ for lam in np.logspace(-8, 2, 10):
         best_loss = this_loss
         best_lam = lam
 
-x = scaler.fit_transform(x)
 X = np.vstack((x, best_lam*np.eye(dim)))
 Y = np.concatenate((y, [0] * dim))
 w, loss = nnls(X, Y)
 
-transf_features = features * w
-transf_dot = transf_features @ transf_features.transpose()
-transf_dot = transf_dot[np.triu(transf_dot, k=1).astype(bool)]
+transf_features = features_120 * w
+np.savetxt('resnet50_transformed_120.txt', transf_features, fmt='%.18f')
+np.savetxt('regression_weights.txt')
+print(best_lam)
 
-sims = pd.DataFrame({'Raw': dotproduct, 'Transformed': transf_dot, 'Observed': obs_sim})
-print(sims.corr())
-
-np.savetxt('resnet50_transformed2_120.txt', transf_features, fmt='%.18f')
-
+raw = x @ np.ones((2048,))
+transf = x @ w
+print(r2_score(y, raw))
+print(r2_score(y, pred))
