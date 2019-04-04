c0d94546ca002af3dc83c59942e340ca6cc670b9
craigsanders <crasanders@gmail.com>
2019-04-04 08:37:55 -0500

    i don't understand what happened, but this is the file that should have been committed

--- predict_similarity.py
+++ predict_similarity.py
@@ -2,18 +2,10 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import nnls
 from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
-
-obs_sim = np.array(pd.read_csv('data/similarity_120.csv', header=None))
-obs_sim = 10 - obs_sim[np.triu(obs_sim, k=1).astype(bool)]
 
 features_360 = np.loadtxt('data/resnet50_features_360.txt')
 features_120 = np.loadtxt('data/resnet50_features_120.txt')
-
-scaler = StandardScaler()
-features_360 = scaler.fit_transform(features_360)
-features_120 = scaler.transform(features_120)
 
 n, dim = features_360.shape
 x = np.array([(features_360[i,:] - features_360[j,:])**2 for i in range(n-1) for j in range(i+1, n)])
@@ -29,6 +21,7 @@ kf = KFold(5)
 best_loss = np.inf
 best_lam = None
 for i, lam in enumerate(np.logspace(2, 3, 10)):
+    print(i)
     this_loss = 0
     for train, val in kf.split(x):
         x_train, x_val = x[train], x[val]
@@ -52,10 +45,20 @@ w, loss = nnls(X, Y)
 
 transf_features = features_120 * w
 np.savetxt('resnet50_transformed_120.txt', transf_features, fmt='%.18f')
-np.savetxt('regression_weights.txt')
+np.savetxt('regression_weights.txt', w, fmt='%.18f')
 print(best_lam)
 
-raw = x @ np.ones((2048,))
-transf = x @ w
-print(r2_score(y, raw))
-print(r2_score(y, pred))
+n, dim = features_120.shape
+dist = np.array([(features_120[i,:] - features_120[j,:])**2 for i in range(n-1) for j in range(i+1, n)])
+transf_dist = np.array([(transf_features[i,:] - transf_features[j,:])**2 for i in range(n-1) for j in range(i+1, n)])
+sim_120 = np.array(pd.read_csv('data/similarity_120.csv', header=None))
+sim_120 = 10 - sim_120[np.triu(sim_120, k=1).astype(bool)]
+
+dist = dist[~np.isnan(sim_120)]
+transf_dist = transf_dist[~np.isnan(sim_120)]
+sim_120 = sim_120[~np.isnan(sim_120)]
+
+raw = np.sqrt(dist @ np.ones((dim,)))
+transf = np.sqrt(transf_dist @ w)
+print(r2_score(sim_120, raw))
+print(r2_score(sim_120, transf))
